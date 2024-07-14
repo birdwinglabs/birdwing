4c1d3862beeda5592dd9c22077d5abb1c01adb14
Björn Andersson <bjorn.olof.andersson@gmail.com>
2024-07-14 08:22:45 +0200

    switch from rollup to esbuild

--- packages/react/src/compiler.ts
+++ packages/react/src/compiler.ts
@@ -1,33 +1,20 @@
-import { rollup } from 'rollup';
-import nodeResolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import babel from '@rollup/plugin-babel';
 import vm from 'vm';
 import path from 'path';
 import { createRequire } from 'module';
 import { fileURLToPath } from 'url';
 import React from 'react';
+import * as esbuild from 'esbuild'
 
 export async function compile(filepath: string, dstRoot: string) {
-  const config: any = {
-    input: filepath,
-    plugins: [
-      nodeResolve({
-        extensions: ['.js', '.jsx'],
-        moduleDirectories: [path.join(dstRoot, 'node_modules')]
-      }),
-      babel({
-        babelHelpers: 'bundled',
-        presets: ['@babel/preset-react'],
-        extensions: ['.js', '.jsx']
-      }),
-      commonjs(),
-    ],
-  }
+  const result = await esbuild.build({
+    entryPoints: [filepath],
+    bundle: true,
+    format: 'cjs',
+    outfile: 'out.js',
+    write: false,
+  })
+  const code = result.outputFiles[0].text;
 
-  const bundle = await rollup(config);
-  const generated = await bundle.generate({ format: 'cjs', exports: 'default' });
-  const code = generated.output[0].code;
   const sandbox = {
     require: createRequire(import.meta.url),
     __dirname: path.dirname(fileURLToPath(import.meta.url)),
@@ -37,7 +24,7 @@ export async function compile(filepath: string, dstRoot: string) {
     React,
   };
 
-  const componentClass = vm.runInNewContext(code, sandbox);
+  const componentClass = vm.runInNewContext(code, sandbox).default;
   const component = new componentClass();
 
   return (node: string) => {
