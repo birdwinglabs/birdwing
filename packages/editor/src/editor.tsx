4910ece9632e85a27d964c67659a1f8fa68f4276
Björn Andersson <bjorn.olof.andersson@gmail.com>
2024-09-18 12:20:34 +0200

    initial support fr different screen sizes in editor

--- packages/editor/src/editor.tsx
+++ packages/editor/src/editor.tsx
@@ -7,6 +7,8 @@ import TextInput from './components/TextInput';
 export default function Editor({ children, source, onChange, onSave }: any) {
   const [content, setContent] = useState(source);
   const [panel, setPanel] = useState('content');
+  const [size, setSize] = useState('desktop');
+  const [panelSize, setPanelSize] = useState(600);
 
   function onChangeEvent(document: SourceDocument) {
     setContent(document);
@@ -21,6 +23,22 @@ export default function Editor({ children, source, onChange, onSave }: any) {
     onChange(c);
   }
 
+  //const panelSize = 600;
+
+  React.useEffect(() => {
+    switch(size) {
+      case 'desktop':
+        setPanelSize(600);
+        break;
+      case 'tablet':
+        setPanelSize(800);
+        break;
+      case 'mobile':
+        setPanelSize(1024);
+        break;
+    }
+  }, [size])
+
   React.useEffect(() => {
     if (source) {
       setContent(source);
@@ -31,9 +49,27 @@ export default function Editor({ children, source, onChange, onSave }: any) {
   const tabInactive = 'px-4 py-2 text-stone-400';
 
   return (
-    <div className="bg-stone-200 w-screen h-screen p-4 pl-[600px] fixed">
-      <div className="fixed h-[calc(100vh-2rem)] w-[600px] left-0">
-        <div className="w-full flex items-center gap-2 pb-4 px-6 border-b text-sm">
+    <div className="bg-stone-200 w-screen h-screen p-4 pt-20 fixed" style={{ paddingLeft: `${panelSize}px`}}>
+      <div className="fixed h-20 top-0 pt-4 pr-4" style={{ width: `calc(100vw - ${panelSize}px)`, left: `${panelSize}px` }}>
+        <div className="w-full h-full flex items-center gap-2 p-4 text-sm bg-white border-b-8 border-stone-200 shadow-xl">
+          <button onClick={() => onSave(content)} className="rounded px-4 py-2 bg-stone-800 text-white">
+            Save
+          </button>
+          <div className="flex items-center ml-auto">
+            <button onClick={() => setSize('desktop')} className={ size === 'desktop' ? tabActive : tabInactive}>
+              Desktop
+            </button>
+            <button onClick={() => setSize('tablet')} className={ size === 'tablet' ? tabActive : tabInactive}>
+              Tablet
+            </button>
+            <button onClick={() => setSize('mobile')} className={ size === 'mobile' ? tabActive : tabInactive}>
+              Mobile
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="fixed h-[calc(100vh-4rem)] left-0 top-0" style={{ width: `${panelSize}px` }}>
+        <div className="w-full flex items-center gap-2 pb-4 px-6 pt-4 border-b text-sm">
           <button className="py-2 mr-2 text-stone-900">
            <span className="material-symbols-outlined">
             arrow_back
@@ -45,11 +81,6 @@ export default function Editor({ children, source, onChange, onSave }: any) {
           <button onClick={() => setPanel('meta')} className={ panel === 'meta' ? tabActive : tabInactive}>
             Meta
           </button>
-          <div className="flex items-center ml-auto">
-            <button onClick={() => onSave(content)} className="rounded px-4 py-2 bg-stone-800 text-white">
-              Save
-            </button>
-          </div>
         </div>
         { panel === 'content' && content &&
           <ContentEditor document={content} onChange={onChangeEvent} />
