f279ded62c85ddadc2e017b08b4fe3b0c590f8f2
Björn Andersson <bjorn.olof.andersson@gmail.com>
2024-12-03 09:09:37 +0100

    use children in code block

--- packages/react/src/CodeBlock.tsx
+++ packages/react/src/CodeBlock.tsx
@@ -2,10 +2,10 @@ import { createContext } from 'react';
 
 export const HighlightContext = createContext<any>(undefined);
 
-export function CodeBlock({ content, language, className }: any) {
+export function CodeBlock({ language, className, children }: any) {
   return (
     <HighlightContext.Consumer>
-      { highlight => <code className={className} dangerouslySetInnerHTML={{ __html: highlight(content, language) }}></code> }
+      { highlight => children && <code className={className} dangerouslySetInnerHTML={{ __html: highlight(children, language) }}></code> }
     </HighlightContext.Consumer>
   );
 }
