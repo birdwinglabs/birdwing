bdcd601d9a4c0f0066feccb3960dae61fd861086
Björn Andersson <bjorn.olof.andersson@gmail.com>
2025-03-11 16:13:51 +0100

    annotate menu

--- packages/renderable/src/schema/menu.ts
+++ packages/renderable/src/schema/menu.ts
@@ -1,9 +1,16 @@
 import { ComponentType } from "../interfaces.js";
+import { LinkItem } from "./common.js";
 
-export class Menu {}
+export class Menu {
+  title: string | undefined = undefined;
+  item: LinkItem[] = [];
+}
 
 export interface MenuComponent extends ComponentType<Menu> {
   tag: 'nav',
-  properties: {},
+  properties: {
+    title: 'h1',
+    item: 'li',
+  },
   refs: {},
 }
--- packages/schema/src/documents/menu.ts
+++ packages/schema/src/documents/menu.ts
@@ -1,8 +1,32 @@
-import { Schema, Tag } from '@markdoc/markdoc';
+import { RenderableTreeNodes } from '@markdoc/markdoc';
+import { schema } from '@birdwing/renderable';
+import { group, Model } from '../lib/index.js';
+import { NodeStream } from '../lib/node.js';
+import { createComponentRenderable, createSchema } from '../lib/index.js';
+import { linkItem } from '../tags/common.js';
 
-export const menu: Schema = {
-  children: ['heading', 'link', 'list'],
-  transform(node, config) {
-    return new Tag('nav', { property: 'menu', typeof: 'Menu' }, node.transformChildren(config));
-  },
+class MenuModel extends Model {
+  @group({ section: 0, include: ['heading'] })
+  header: NodeStream;
+
+  @group({ section: 0, include: ['list'] })
+  items: NodeStream;
+
+  transform(): RenderableTreeNodes {
+    const children = this.transformChildren({
+      item: linkItem,
+    });
+
+    return createComponentRenderable(schema.Menu, {
+      tag: 'nav',
+      property: 'menu',
+      properties: {
+        title: children.tag('h1').limit(1),
+        item: children.flatten().tag('li'),
+      },
+      children: children.toArray(),
+    });
+  }
 }
+
+export const menu = createSchema(MenuModel);
