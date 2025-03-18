bb3729ab731c3f321fcdccce8edd4422369f03cf
Björn Andersson <bjorn.olof.andersson@gmail.com>
2025-03-18 10:12:42 +0100

    handle data-codeblock in imago code element

--- packages/imago/src/Elements.tsx
+++ packages/imago/src/Elements.tsx
@@ -7,6 +7,11 @@ export const defaultElements: Partial<Record<NodeType, ImagoHandler>> = {
   document: ({ children, className }: any) => {
     return <article className={className}>{ children }</article>;
   },
+  code: ({ className, children, ...props }: any) => {
+    return props['data-codeblock']
+      ? <code className={className} dangerouslySetInnerHTML={{ __html: children }}></code>
+      : <code className={className}>{ children }</code>
+  },
   a({ href, children, nav, ...props }) {
     return nav === true
       ? <NavLink to={href} {...props}>{ children }</NavLink>
